c4434f8535df19235a66147762142d4f476930f9
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-09-23 18:55:59 +0900

    feat : Spring security 로그인 인증권한 검사(**Provider), UserDetail 구현체

---
+++ src/main/java/com/kidsnara/library/security/CustomUserDetails.java
@@ -0,0 +1,88 @@
+package com.kidsnara.library.security;
+
+import com.kidsnara.library.domain.user.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+
+    public CustomUserDetails(User user){
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.role = user.getRole().getValue();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        auth.add(new SimpleGrantedAuthority(role));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    /**
+     * 계정 만료 여부
+     * true : 만료 안됨
+     * false : 만료
+     * @return
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * 계정 잠김 여부
+     * true : 잠기지 않음
+     * false : 잠김
+     * @return
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * 비밀번호 만료 여부
+     * true : 만료 안됨
+     * false : 만료
+     * @return
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * 사용자 활성화 여부
+     * ture : 활성화
+     * false : 비활성화
+     * @return
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/kidsnara/library/security/provider/FormLoginAuthenticationProvider.java
@@ -0,0 +1,56 @@
+package com.kidsnara.library.security.provider;
+
+import com.kidsnara.library.domain.user.UserServiceImpl;
+import com.kidsnara.library.security.CustomUserDetails;
+import com.kidsnara.library.security.token.PostAuthorizationToken;
+import com.kidsnara.library.security.token.PreAuthorizationToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
+
+/**
+ * 로그인 한 사용자의 인증 권한 검사
+ */
+@Component
+@RequiredArgsConstructor
+public class FormLoginAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserServiceImpl userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        PreAuthorizationToken token = (PreAuthorizationToken) authentication;
+
+        String email = token.getEmail();
+        String password = token.getUserPassword();
+
+        CustomUserDetails userDetails = userService.loadUserByUsername(email);
+
+        if(isCorrectPassword(password, userDetails.getPassword())){
+            return PostAuthorizationToken
+                    .getTokenFormUserDetails(userDetails);
+        }
+
+        throw new NoSuchElementException("인증 정보가 정확하지 않습니다.");
+    }
+
+    /*
+    * Provider를 연결해 주는 메서드
+    * PreAuthorizationToken을 사용한 filter를 검색 후 연결
+    * */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return PreAuthorizationToken.class.isAssignableFrom(authentication);
+    }
+
+    private boolean isCorrectPassword(String password, String accountPassword){
+        return passwordEncoder.matches(password, accountPassword);
+    }
+}
