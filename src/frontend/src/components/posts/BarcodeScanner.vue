95e6f667f99b3ff5b1f52a4c2ec849260ea6db11
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-11 20:42:27 +0900

    feat :[front] 바코드 스캐너 구현

---
+++ src/frontend/src/components/posts/BarcodeScanner.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Vue Barcode Reader Demo</h1>
+  <CameraScanner />
+</template>
+
+<script>
+import CameraScanner from "../layout/CameraScanner";
+export default {
+  name: "BarcodeScanner",
+  components: {CameraScanner}
+}
+</script>
+
+<style scoped>
+
+</style>
