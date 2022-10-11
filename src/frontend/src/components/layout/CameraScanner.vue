52eb9a3392546bd5fba71c87a8e839f04fb1747d
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-11 20:41:59 +0900

    feat :[front] 바코드 스캐너 구현

---
+++ src/frontend/src/components/layout/CameraScanner.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+import { StreamBarcodeReader } from "vue-barcode-reader";
+const decodedText = ref("");
+const onLoaded = () => {
+  console.log("loaded");
+};
+const onDecode = (text) => {
+  decodedText.value = text;
+
+  searchData(text);
+};
+
+function searchData(result) {
+  this.emitter.emit("searchData", result);
+}
+
+</script>
+
+<template>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  <h2>The decoded value in QR/barcode is</h2>
+  <h2>{{ decodedText }}</h2>
+
+
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
