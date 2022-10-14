95353917e35293684091cc69cfa290343f916858
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-14 17:05:37 +0900

    refactor : 사용하지 않는 import 제거

--- src/main/java/com/kidsnara/library/constant/Role.java
+++ src/main/java/com/kidsnara/library/constant/Role.java
@@ -1,7 +1,5 @@
 package com.kidsnara.library.constant;
 
-import org.springframework.security.core.GrantedAuthority;
-
 public enum Role implements EnumModel{
     ADMIN("ROLE_ADMIN"),
     USER("ROLE_USER");
