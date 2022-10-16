8642b79f568cbe0b33c76474f25926723d58638f
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-16 22:45:30 +0900

    fix : 로그인 입력값 null일 때 jwt 관련 에러 발생
    원인: LoginRequest의 기본 생성자가 없어서 발생
    참고: 오류는 해결되었으나 로그인시 실행되지 않아야 할 JWTFilter에 해당 오류가 걸린 것은 더 살펴봐야 함.

--- src/main/java/com/kidsnara/library/dto/user/LogInRequest.java
+++ src/main/java/com/kidsnara/library/dto/user/LogInRequest.java
@@ -1,29 +1,28 @@
 package com.kidsnara.library.dto.user;
 
-import com.kidsnara.library.constant.Role;
 import com.kidsnara.library.domain.user.User;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LogInRequest {
 
     @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "이메일 형식으로 입력해주세요.")
     @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
-
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    private final Role role = Role.ADMIN;
 
     public User toEntity() {
         return User.builder()
+                .email(this.email)
                 .password(this.password)
-                .role(this.role)
                 .build();
     }
 }
