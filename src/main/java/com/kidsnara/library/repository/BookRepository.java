8cb95ef54d5ce17726d288c1826a0d99ddf6a34f
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-20 19:52:07 +0900

    refactor : 도서 목록 Slice >> Page로 변경

--- src/main/java/com/kidsnara/library/repository/BookRepository.java
+++ src/main/java/com/kidsnara/library/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.kidsnara.library.repository;
 
 import com.kidsnara.library.domain.library.Book;
 import com.kidsnara.library.dto.book.BookGetRes;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByIsbn(String isbn);
 
     @Query("select new com.kidsnara.library.dto.book.BookGetRes(b.id, b.title, b.author, b.possibleCnt) from Book b ")
-    Slice<BookGetRes> findBookGetRes(Pageable pageable);
+    Page<BookGetRes> findBookGetRes(Pageable pageable);
 }
