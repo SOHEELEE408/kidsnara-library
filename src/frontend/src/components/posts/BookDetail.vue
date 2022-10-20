c0167431f6c6a216d9986e0b08feb79e5b9a00a7
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-10-20 19:47:50 +0900

    fix : [front] 도서 상세 조회 페이지 구현
    - 버튼 위치 등 ui 수정 필요

--- src/frontend/src/components/posts/BookDetail.vue
+++ src/frontend/src/components/posts/BookDetail.vue
@@ -2,15 +2,19 @@
   <div class="container">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">게시글 상세</h5>
-        <p class="card-text">
-          <small class="text-muted">작성자 : {{posts.author}} ({{posts.createdDate}}}</small>
-        </p>
-
+        <h5 class="card-title">도서 상세</h5>
         <form>
 
           <div class="form-group">
-            <label for="postsTitle">제목</label>
+            <label for="isbn">ISBN</label>
+            <input type="text"
+                   class="form-control"
+                   id="isbn"
+                   v-model="posts.isbn"
+                   :disabled="disabled"/>
+          </div>
+          <div class="form-group">
+            <label for="postsTitle">도서명</label>
             <input type="text"
                    class="form-control"
                    id="postsTitle"
@@ -18,15 +22,52 @@
                    v-model="posts.title"
                    :disabled="disabled"/>
           </div>
+          <div class="form-group">
+            <label for="postsAuthor">작가</label>
+            <input type="text"
+                   class="form-control"
+                   id="postsAuthor"
+                   v-model="posts.author"
+                   placeholder="작가"
+                   :disabled="disabled"/>
+          </div>
 
           <div class="form-group">
-            <label for="postsContent">본문</label>
-            <textarea rows="10"
-                      class="form-control"
-                      id="postsContent"
-                      placeholder="본문 내용을 입력해주세요."
-                      v-model="posts.content"
-                      :disabled="disabled">작성글 본문 테스트 중입니다.</textarea>
+            <label for="postsPublisher">출판사</label>
+            <input type="text"
+                   class="form-control"
+                   id="postsPublisher"
+                   v-model="posts.publisher"
+                   placeholder="출판사"
+                   :disabled="disabled"/>
+          </div>
+
+          <div class="form-group">
+            <label for="postsDiscount">가격</label>
+            <input type="text"
+                   class="form-control"
+                   id="postsDiscount"
+                   v-model="posts.discount"
+                   placeholder="가격"
+                   :disabled="disabled"/>
+          </div>
+
+          <div class="form-group">
+            <label for="postsCount">보유 권 수</label>
+            <input type="number"
+                   class="form-control"
+                   id="postsCount"
+                   v-model="posts.count"
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="postsGenre">장르</label>
+            <input type="text"
+                   class="form-control"
+                   id="postsGenre"
+                   v-model="posts.genre"
+                   placeholder="장르"/>
           </div>
 
         </form>
@@ -35,7 +76,7 @@
 
     <div class="row mt-3 float-left">
       <div class="col-auto">
-        <router-link :to="{path:'/posts'}"
+        <router-link :to="{path:'/books'}"
                      class="btn btn-primary">
           <i class="fa">목록으로</i>
         </router-link>
@@ -47,7 +88,7 @@
       <div class="col-auto">
         <button class="btn btn-success"
                 type="button"
-                @click="setMode()">수정</button>
+                @click="setPosts()">수정</button>
       </div>
 
       <div class="col-auto">
@@ -55,17 +96,6 @@
             class="btn btn-danger"
             type="button"
             @click="delPosts()">삭제
-        </button>
-      </div>
-    </div>
-
-    <div class="row mt-3 float-right"
-         v-if="!disabled">
-      <div class="col-auto">
-        <button
-            class="btn btn-success"
-            type="button"
-            @click="setPosts()">저장
         </button>
       </div>
 
@@ -82,11 +112,11 @@
 
 <script>
 export default {
-  name: "PostsDetail",
+  name: "BookDetail",
   data(){
     return{
       disabled:true,
-      param:this.$route.query,
+      bookId: this.$route.params.bookId,
       posts:{}
     }
   },
@@ -105,38 +135,45 @@ export default {
       this.disabled=false
     },
     getPosts(){
-      this.axios.get('http://localhost:9000/api/v1/posts/'+this.param.id)
+      this.axios.get('http://localhost:8081/books/'+this.bookId,
+
+          {headers: { 'X-ACCESS-TOKEN': localStorage.getItem('token')}}
+      )
       .then(res=>{
+        console.log(res.data)
         this.posts=res.data
       }).catch(e=>{
-        console.log(e)
+        console.log(e.response.data)
       })
     },
     setPosts(){
       let params={
-        "id": this.param.id,
-        "author":this.posts.author,
-        "title":this.posts.title,
-        "content":this.posts.content,
+        "count": this.posts.count,
+        "genre": this.posts.genre
       }
-      this.axios.put('http://localhost:9000/api/v1/posts/'+this.param.id,
-      JSON.stringify(params), {headers:{'content-type': 'application/json'}}
+      this.axios.patch('http://localhost:8081/books/'+this.bookId,
+      params,
+          {headers: {
+              'X-ACCESS-TOKEN': localStorage.getItem('token'),
+              'Content-Type':'application/json;charset=UTF-8'
+            }}
       ).then(res=>{
         console.log(res.data)
         alert("수정되었습니다.")
         this.handleService()
       }).catch(e=>{
-        alert(e.response.data)
+        alert(e.response.data.message)
       })
     },
     delPosts(){
-      this.axios.delete('http://localhost:9000/api/v1/posts/'+this.param.id
+      this.axios.delete('http://localhost:8081/books/'+this.bookId,
+          {headers: { 'X-ACCESS-TOKEN': localStorage.getItem('token')}}
       ).then(res=>{
         console.log(res.data)
         alert("삭제되었습니다.")
-        this.$router.push("/posts")
+        this.$router.push("/books")
       }).catch(e=>{
-        alert(e.response.data)
+        alert(e.response.data.message)
       })
     }
   }
